ee2aece34642363e9c35be7b571e935ed1c0d347
aalexanderfc <144456869+aalexanderfc@users.noreply.github.com>
2024-09-17 11:29:52 +0200

    first

---
+++ src/main/java/com/example/systemintegrationiot/SystemintegrationIoTApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemintegrationiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemintegrationIoTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemintegrationIoTApplication.class, args);
+    }
+
+}
