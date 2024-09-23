98b166e8f273aab355db342c5a686749f18c2a77
Epic41z0 <alialzobaydee@hotmail.com>
2024-09-23 14:59:49 +0200

    Skapat Main och index.html samt kopplat Sensordata till html. // Ali Al-Zobaydee

--- src/main/java/com/example/systemintegrationiot/SystemintegrationIoTApplication.java
+++ src/main/java/com/example/systemintegrationiot/SystemintegrationIoTApplication.java
@@ -1,5 +1,6 @@
 package com.example.systemintegrationiot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class SystemintegrationIoTApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemintegrationIoTApplication.class, args);
     }
-
 }
