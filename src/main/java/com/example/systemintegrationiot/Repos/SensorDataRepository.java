75470fb4b8596ed39438afec948bd4a2f6aeaac8
aalexanderfc <144456869+aalexanderfc@users.noreply.github.com>
2024-09-23 14:39:11 +0200

    trying to connect esp32 data till spring boot(not complete)

--- src/main/java/com/example/systemintegrationiot/Repos/SensorDataRepository.java
+++ src/main/java/com/example/systemintegrationiot/Repos/SensorDataRepository.java
@@ -2,6 +2,7 @@ package com.example.systemintegrationiot.Repos;
 
 import com.example.systemintegrationiot.Data.SensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
 }
