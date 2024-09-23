e0176a0b21cd0eb02435837c40b38ef69ef9376f
Epic41z0 <alialzobaydee@hotmail.com>
2024-09-23 18:06:18 +0200

    Skapat Main och index.html samt kopplat Sensordata till html. // Ali Al-Zobaydee

--- src/main/java/com/example/systemintegrationiot/Controller/SensorController.java
+++ src/main/java/com/example/systemintegrationiot/Controller/SensorController.java
@@ -3,15 +3,10 @@ package com.example.systemintegrationiot.Controller;
 import com.example.systemintegrationiot.Data.SensorData;
 import com.example.systemintegrationiot.Service.SensorDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@Controller  // Change to @Controller to serve the Thymeleaf view
+@Service
 public class SensorController {
 
     private final SensorDataService service;
@@ -21,17 +16,9 @@ public class SensorController {
         this.service = sensorService;
     }
 
-    // Endpoint for saving sensor data (POST request)
-    @PostMapping("/data")
+    @PostMapping("/sensor_data")
     public SensorData saveSensorData(@RequestParam double temperature, @RequestParam double humidity) {
         return service.saveSensorData(temperature, humidity);
     }
-
-    // Endpoint to display sensor data (GET request)
-    @GetMapping("/data")  // Here is your /data GET request for fetching data
-    public String showSensorData(Model model) {
-        List<SensorData> sensorDataList = service.getAllSensorData();
-        model.addAttribute("sensorDataList", sensorDataList);
-        return "index";  // Renders the Thymeleaf template (index.html)
-    }
 }
+
