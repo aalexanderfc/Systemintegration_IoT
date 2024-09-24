0b04ab90073fc3b3b5bfccdcff7221107478663b
aalexanderfc <144456869+aalexanderfc@users.noreply.github.com>
2024-09-24 11:19:50 +0200

    I've moved folders to com.example.systemintegrationIot

--- src/main/java/com/example/systemintegrationiot/Repos/SensorDataRepository.java
+++ src/main/java/com/example/systemintegrationiot/Repos/SensorDataRepository.java
@@ -2,7 +2,6 @@ package com.example.systemintegrationiot.Repos;
 
 import com.example.systemintegrationiot.Data.SensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
 }
